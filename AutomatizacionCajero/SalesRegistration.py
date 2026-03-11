b480c7ed15d5f896e811591a349bab3e69f13a25
Juan Bolivar <jebolivarp23@gmail.com>
2026-03-11 15:28:06 -0500

    ARREGLAR TRY EN EL SalesRegistration

--- AutomatizacionCajero/SalesRegistration.py
+++ AutomatizacionCajero/SalesRegistration.py
@@ -2,13 +2,27 @@ def SalesRegistration():
     VALIDATOR = True
     while VALIDATOR:  
         try:
-            ProductName = input("Enter the product name: ")  
-            ProductPrice = float(input("Enter the product price: "))
-            ProductQuantity = int(input("How many products do you want to buy?: "))
-            Total = ProductPrice * ProductQuantity  
-            VALIDATOR = False
-            # Returning the data as a tuple
-            return (ProductName, ProductPrice, ProductQuantity, Total)
-        except ValueError:       
-            print("ERROR! You must enter a number. Try again.")
-            
+            ProductName = input("Enter the product name: ")
+            if ProductName.strip()=="":
+                int("Error: Empty Name")
+            else:
+                try:
+                    ProductPrice = float(input("Enter the product price: "))
+                    
+                    if ProductPrice < 0:
+                        # Forzamos un error de valor convirtiendo algo que no es número
+                        int("Error: Negative") 
+                    else:
+                        ProductQuantity = int(input("How many products do you want to buy?: "))
+                        
+                        if ProductQuantity < 0:
+                            int("Error: Negative Quantity")
+                        else:
+
+                            Total = ProductPrice * ProductQuantity  
+                            VALIDATOR = False
+                            return (ProductName, ProductPrice, ProductQuantity, Total)
+                except ValueError:
+                    print("ERROR! Invalid input (it must be a positive number). Try again.")
+        except ValueError:
+            print("ERROR! Invalid input (it must be a positive number). Try again.")
